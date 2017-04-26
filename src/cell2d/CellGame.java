ce66c4401656bc6a4ffbf9422e27e57361efadd4
742mph <abheyman23@sbcglobal.net>
2017-04-25 22:38:25 -0500

    (Officially) began work on version 1.1.1

--- src/cell2d/CellGame.java
+++ src/cell2d/CellGame.java
@@ -75,9 +75,9 @@ import org.newdawn.slick.util.Log;
 public abstract class CellGame {
     
     /**
-     * The version number of Cell2D, currently 1.1.0.
+     * The version number of Cell2D, currently 1.1.1.
      */
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.1.1";
     
     private static enum CommandState {
         NOTHELD, PRESSED, HELD, RELEASED, TAPPED, UNTAPPED
