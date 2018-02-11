de231876c390c7a6a35545aa08ed9800519545bf
Andrew Heyman <abheyman23@sbcglobal.net>
2018-02-11 15:00:14 -0500

    Clarified Drawable's javadoc

--- src/cell2d/Drawable.java
+++ src/cell2d/Drawable.java
@@ -5,33 +5,36 @@ import org.newdawn.slick.Graphics;
 /**
  * <p>A Drawable object is one that visually represents itself with an image
  * that is contextless by itself but can be drawn to a Graphics context. Points
- * on the image have x-coordinates, which are 0 at its left edge and increase
- * from left to right, and y-coordinates, which are 0 at its top edge and
- * increase from top to bottom. The drawn image has an origin point somewhere on
- * (or off) it around which it is flipped, rotated, and scaled.</p>
+ * on the image have x-coordinates that increase from left to right, as well as
+ * y-coordinates that increase from top to bottom. The image has an origin point
+ * somewhere on (or off) it around which it is flipped, rotated, and scaled.</p>
  * @author Andrew Heyman
  */
 public interface Drawable {
     
     /**
-     * Draws this Drawable to the specified Graphics context.
-     * @param g The Graphics context to draw this Drawable to
-     * @param x The x-coordinate on the Graphics context of the origin
-     * @param y The y-coordinate on the Graphics context of the origin
+     * Draws this Drawable's image to the specified Graphics context.
+     * @param g The Graphics context to draw the image to
+     * @param x The x-coordinate on the Graphics context of the drawn image's
+     * origin
+     * @param y The y-coordinate on the Graphics context of the drawn image's
+     * origin
      */
     public abstract void draw(Graphics g, int x, int y);
     
     /**
-     * Draws this Drawable to the specified Graphics context.
-     * @param g The Graphics context to draw this Drawable to
-     * @param x The x-coordinate on the Graphics context of the origin
-     * @param y The y-coordinate on the Graphics context of the origin
+     * Draws this Drawable's image to the specified Graphics context.
+     * @param g The Graphics context to draw the image to
+     * @param x The x-coordinate on the Graphics context of the drawn image's
+     * origin
+     * @param y The y-coordinate on the Graphics context of the drawn image's
+     * origin
      * @param xFlip If true, the drawn image is flipped along a vertical line
-     * through the origin
+     * through its origin
      * @param yFlip If true, the drawn image is flipped along a horizontal line
-     * through the origin
+     * through its origin
      * @param angle The angle in degrees by which to rotate the drawn image and
-     * its xFlip and yFlip lines counterclockwise around the origin
+     * its xFlip and yFlip lines counterclockwise around its origin
      * @param alpha The drawn image's alpha (opacity) value from 0 to 1
      * @param filter The Filter to apply to the drawn image, or null if none
      * should be applied. Not every Filter has an effect on every Drawable.
@@ -40,16 +43,18 @@ public interface Drawable {
             boolean xFlip, boolean yFlip, double angle, double alpha, Filter filter);
     
     /**
-     * Draws this Drawable to the specified Graphics context.
-     * @param g The Graphics context to draw this Drawable to
-     * @param x The x-coordinate on the Graphics context of the origin
-     * @param y The y-coordinate on the Graphics context of the origin
-     * @param scale The factor by which to scale the drawn image around the
+     * Draws this Drawable's image to the specified Graphics context.
+     * @param g The Graphics context to draw the image to
+     * @param x The x-coordinate on the Graphics context of the drawn image's
+     * origin
+     * @param y The y-coordinate on the Graphics context of the drawn image's
+     * origin
+     * @param scale The factor by which to scale the drawn image around its
      * origin
      * @param xFlip If true, the drawn image is flipped along a vertical line
-     * through the origin
+     * through its origin
      * @param yFlip If true, the drawn image is flipped along a horizontal line
-     * through the origin
+     * through its origin
      * @param alpha The drawn image's alpha (opacity) value from 0 to 1
      * @param filter The Filter to apply to the drawn image, or null if none
      * should be applied. Not every Filter has an effect on every Drawable.
@@ -58,36 +63,36 @@ public interface Drawable {
             boolean xFlip, boolean yFlip, double alpha, Filter filter);
     
     /**
-     * Draws a rectangular region of this Drawable to the specified Graphics
-     * context.
-     * @param g The Graphics context to draw this Drawable to
-     * @param x The x-coordinate on the Graphics context of the origin
-     * @param y The y-coordinate on the Graphics context of the origin
-     * @param left The x-coordinate on the image, relative to the origin, of the
-     * drawn region's left edge
-     * @param right The x-coordinate on the image, relative to the origin, of
-     * the drawn region's right edge
-     * @param top The y-coordinate on the image, relative to the origin, of the
-     * drawn region's top edge
-     * @param bottom The y-coordinate on the image, relative to the origin, of
-     * the drawn region's bottom edge
+     * Draws a rectangular region of this Drawable's image to the specified
+     * Graphics context.
+     * @param g The Graphics context to draw the region to
+     * @param x The x-coordinate on the Graphics context of the image's origin
+     * @param y The y-coordinate on the Graphics context of the image's origin
+     * @param left The x-coordinate on the image, relative to its origin, of the
+     * region's left edge
+     * @param right The x-coordinate on the image, relative to its origin, of
+     * the region's right edge
+     * @param top The y-coordinate on the image, relative to its origin, of the
+     * region's top edge
+     * @param bottom The y-coordinate on the image, relative to its origin, of
+     * the region's bottom edge
      */
     public abstract void draw(Graphics g, int x, int y, int left, int right, int top, int bottom);
     
     /**
-     * Draws a rectangular region of this Drawable to the specified Graphics
-     * context.
-     * @param g The Graphics context to draw this Drawable to
-     * @param x The x-coordinate on the Graphics context of the origin
-     * @param y The y-coordinate on the Graphics context of the origin
-     * @param left The x-coordinate on the image, relative to the origin, of the
-     * drawn region's left edge
-     * @param right The x-coordinate on the image, relative to the origin, of
-     * the drawn region's right edge
-     * @param top The y-coordinate on the image, relative to the origin, of the
-     * drawn region's top edge
-     * @param bottom The y-coordinate on the image, relative to the origin, of
-     * the drawn region's bottom edge
+     * Draws a rectangular region of this Drawable's image to the specified
+     * Graphics context.
+     * @param g The Graphics context to draw the region to
+     * @param x The x-coordinate on the Graphics context of the image's origin
+     * @param y The y-coordinate on the Graphics context of the image's origin
+     * @param left The x-coordinate on the image, relative to its origin, of the
+     * region's left edge
+     * @param right The x-coordinate on the image, relative to its origin, of
+     * the region's right edge
+     * @param top The y-coordinate on the image, relative to its origin, of the
+     * region's top edge
+     * @param bottom The y-coordinate on the image, relative to its origin, of
+     * the region's bottom edge
      * @param xFlip If true, the drawn region is flipped along a vertical line
      * through the image's origin
      * @param yFlip If true, the drawn region is flipped along a horizontal line
@@ -102,21 +107,21 @@ public interface Drawable {
             boolean xFlip, boolean yFlip, double angle, double alpha, Filter filter);
     
     /**
-     * Draws a rectangular region of this Drawable to the specified Graphics
-     * context.
-     * @param g The Graphics context to draw this Drawable to
-     * @param x The x-coordinate on the Graphics context of the origin
-     * @param y The y-coordinate on the Graphics context of the origin
-     * @param left The x-coordinate on the image, relative to the origin, of the
-     * drawn region's left edge
-     * @param right The x-coordinate on the image, relative to the origin, of
-     * the drawn region's right edge
-     * @param top The y-coordinate on the image, relative to the origin, of the
-     * drawn region's top edge
-     * @param bottom The y-coordinate on the image, relative to the origin, of
-     * the drawn region's bottom edge
+     * Draws a rectangular region of this Drawable's image to the specified
+     * Graphics context.
+     * @param g The Graphics context to draw the region to
+     * @param x The x-coordinate on the Graphics context of the image's origin
+     * @param y The y-coordinate on the Graphics context of the image's origin
+     * @param left The x-coordinate on the image, relative to its origin, of the
+     * region's left edge
+     * @param right The x-coordinate on the image, relative to its origin, of
+     * the region's right edge
+     * @param top The y-coordinate on the image, relative to its origin, of the
+     * region's top edge
+     * @param bottom The y-coordinate on the image, relative to its origin, of
+     * the region's bottom edge
      * @param scale The factor by which to scale the drawn region around the
-     * origin
+     * image's origin
      * @param xFlip If true, the drawn region is flipped along a vertical line
      * through the image's origin
      * @param yFlip If true, the drawn region is flipped along a horizontal line
