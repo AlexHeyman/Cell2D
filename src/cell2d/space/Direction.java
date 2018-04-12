232979ea42544127584f8fbcacc922c525f02048
Andrew Heyman <abheyman23@sbcglobal.net>
2018-04-12 17:09:56 -0500

    Added rotation and reflection methods to Direction

--- src/cell2d/space/Direction.java
+++ src/cell2d/space/Direction.java
@@ -20,5 +20,60 @@ public enum Direction {
     /**
      * Down.
      */
-    DOWN
+    DOWN;
+    
+    /**
+     * Returns the Direction opposite this one. For instance, RIGHT.opposite()
+     * returns LEFT.
+     * @return The Direction opposite this one
+     */
+    public Direction opposite() {
+        switch (this) {
+            case LEFT:
+                return RIGHT;
+            case RIGHT:
+                return LEFT;
+            case UP:
+                return DOWN;
+            default:
+                return UP;
+        }
+    }
+    
+    /**
+     * Returns the Direction 90 degrees clockwise from this one. For instance,
+     * RIGHT.clockwise() returns DOWN.
+     * @return The Direction 90 degrees clockwise from this one
+     */
+    public Direction clockwise() {
+        switch (this) {
+            case LEFT:
+                return UP;
+            case UP:
+                return RIGHT;
+            case RIGHT:
+                return DOWN;
+            default:
+                return LEFT;
+        }
+    }
+    
+    /**
+     * Returns the Direction 90 degrees counterclockwise from this one. For
+     * instance, RIGHT.counterclockwise() returns UP.
+     * @return The Direction 90 degrees counterclockwise from this one
+     */
+    public Direction counterclockwise() {
+        switch (this) {
+            case LEFT:
+                return DOWN;
+            case DOWN:
+                return RIGHT;
+            case RIGHT:
+                return UP;
+            default:
+                return LEFT;
+        }
+    }
+    
 }
